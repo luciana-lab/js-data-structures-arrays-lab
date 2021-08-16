9023c5e53c81f20415bb25f84fed894273c50ab6
Luciana Sugeta <lulusugeta@gmail.com>
2021-08-16 13:59:14 -0400

    commit tree 9f395525eeb95bb14d71230bd2a3e02346a719a3

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push("Odie"); //add in the end
+prepend.unshift("Odie"); //add in the beginning
+removeLast.pop(); //remove the last element
+removeFirst.shift(); //remove the first element
